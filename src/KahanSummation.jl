cbbcbccaa2a7a5c16b6c668afe16891b7af5d9ce
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-12 14:18:59 -0500

    maybe ok

--- src/KahanSummation.jl
+++ src/KahanSummation.jl
@@ -64,20 +64,72 @@ function cumsum_kbn(A::AbstractArray{T}, axis::Integer) where T<:AbstractFloat
     return B + C
 end
 
-function cumsum_kbn(v::AbstractVector{T}) where T<:AbstractFloat
-    r = similar(v)ou can just use BenchmarkTools’ built-in setup argument which is always run before the code to be timed:
+if VERSION >= v"0.7.0-DEV.3000" # TODO: More specific bound
+    if isdefined(Base, :sum_kbn) # Deprecated
+        import Base: sum_kbn, cumsum_kbn
+    else
+        export sum_kbn, cumsum_kbn
+    end
+end
 
-@btime sort!(x) setup=(x = rand(100)) evals=1
-Note that evals=1 is important to ensure that the sort!(x) call is run exactly once after each setup call.
+if isdefined(Base, Symbol("@default_eltype"))
+    using Base: @default_eltype
+else
+    macro default_eltype(itr)
+        quote
+            Core.Inference.return_type(first, Tuple{$(esc(itr))})
+        end
+    end
+end
+
+if isdefined(Base, :promote_sys_size_add)
+    using Base: promote_sys_size_add
+else
+    promote_sys_size_add(x::T) where {T} = Base.r_promote(+, zero(T)::T)
+end
+
+"""
+    cumsum_kbn(A, dim::Integer)
+Cumulative sum along a dimension, using the Kahan-Babuska-Neumaier compensated summation
+algorithm for additional accuracy.
+"""
+function cumsum_kbn(A::AbstractArray{T}, axis::Integer) where T<:AbstractFloat
+    dimsA = size(A)
+    ndimsA = ndims(A)
+    axis_size = dimsA[axis]
+    axis_stride = 1
+    for i = 1:(axis-1)
+        axis_stride *= size(A, i)
+    end
+    axis_size <= 1 && return A
+    B = similar(A)
+    C = similar(A)
+    for i = 1:length(A)
+        if div(i-1, axis_stride) % axis_size == 0
+            B[i] = A[i]
+            C[i] = zero(T)
+        else
+            s = B[i-axis_stride]
+            Ai = A[i]
+            B[i] = t = s + Ai
+            if abs(s) >= abs(Ai)
+                C[i] = C[i-axis_stride] + ((s-t) + Ai)
+            else
+                C[i] = C[i-axis_stride] + ((Ai-t) + s)
+            end
+        end
+    end
+    return B + C
+end
+
+function cumsum_kbn(v::AbstractVector{T}) where T<:AbstractFloat
+    r = similar(v)
     isempty(v) && return r
     inds = indices(v, 1)
     i1 = first(inds)
     s = r[i1] = v[i1]
     c = zero(T)
-    for i = i1+1:last(inds)ou can just use BenchmarkTools’ built-in setup argument which is always run before the code to be timed:
-
-@btime sort!(x) setup=(x = rand(100)) evals=1
-Note that evals=1 is important to ensure that the sort!(x) call is run exactly once after each setup call.
+    for i = i1+1:last(inds)
         vi = v[i]
         t = s + vi
         if abs(s) >= abs(vi)
