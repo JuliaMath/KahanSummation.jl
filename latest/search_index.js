b36934de76d7095e68e5289f4a2df2330cbf1010
autodocs <autodocs>
2018-08-27 18:36:18 +0000

    build based on 1ad8910

--- latest/search_index.js
+++ latest/search_index.js
@@ -12,7 +12,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#KahanSummation.sum_kbn",
     "page": "Home",
     "title": "KahanSummation.sum_kbn",
-    "category": "Function",
+    "category": "function",
     "text": "sum_kbn(A)\n\nReturn the sum of all elements of A, using the Kahan-Babuska-Neumaier compensated summation algorithm for additional accuracy.\n\n\n\n"
 },
 
@@ -20,7 +20,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#KahanSummation.cumsum_kbn",
     "page": "Home",
     "title": "KahanSummation.cumsum_kbn",
-    "category": "Function",
+    "category": "function",
     "text": "cumsum_kbn(A, dim::Integer)\n\nCumulative sum along a dimension, using the Kahan-Babuska-Neumaier compensated summation algorithm for additional accuracy.\n\n\n\n"
 },
 
@@ -29,7 +29,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "KahanSummation.jl",
     "category": "section",
-    "text": "This package uses the Kahan-Babuska-Neumaier (KBN) compensated summation algorithm for computing sums and cumulative sums. The sum and cumsum functions in Julia's Base library use pairwise summation, which provides high accuracy and good performance. The KBN algorithm significantly reduces numerical error at the cost of performance and memory efficiency.KahanSummation.sum_kbn\nKahanSummation.cumsum_kbn"
+    "text": "This package uses the Kahan-Babuska-Neumaier (KBN) compensated summation algorithm for computing sums and cumulative sums. The sum and cumsum functions in Julia\'s Base library use pairwise summation, which provides high accuracy and good performance. The KBN algorithm significantly reduces numerical error at the cost of performance and memory efficiency.KahanSummation.sum_kbn\nKahanSummation.cumsum_kbn"
 },
 
 ]}
